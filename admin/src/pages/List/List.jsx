4eb69e32a23a7970b99a20a4c48571198a67145c
mukeshkr59 <mukeshkv20@iitk.ac.in>
2025-11-14 08:17:04 +0530

    Admin frontend+backend done

---
+++ admin/src/pages/List/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './List.css'
+
+const List = () => {
+  return (
+    <div>
+      LIST
+    </div>
+  )
+}
+
+export default List
---
+++ admin/src/pages/Orders/Orders.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Orders.css'
+
+const Orders = () => {
+  return (
+    <div>
+      ORDERS
+    </div>
+  )
+}
+
+export default Orders
